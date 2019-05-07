199497c1011c2a0ebabd2821e563cb52eec13fda
Dhaval <dhaval48@gmail.com>
2019-05-07 18:10:42 +0530

    remove hasapitoken

--- src/Models/User.php
+++ src/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace Ongoingcloud\Laravelcrud\Models;
 
-use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -10,7 +9,7 @@ use Lang;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, Notifiable;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
