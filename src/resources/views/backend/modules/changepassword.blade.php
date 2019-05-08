7e5169563a63b93ffffdb878c54b1800581bfd35
Dhaval <dhaval48@gmail.com>
2019-05-08 18:20:35 +0530

    remove section from view blade php

--- src/resources/views/backend/modules/changepassword.blade.php
+++ src/resources/views/backend/modules/changepassword.blade.php
@@ -2,24 +2,22 @@
 
 @section('content')
 <div class="container">
-    <section>
-        <div class="row justify-content-center">
-            <div class="col-md-12 mtop-10">
-                <div class="card">
-                    <div class="card-header">
-                        <h5>
-                            <a class="card-title" href="{{ $data['list_route'] }}"> 
-                            {{ $data['lang']['change_password'] }}
-                            </a>
-                        </h5>
-                    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-12 mtop-10">
+            <div class="card">
+                <div class="card-header">
+                    <h5>
+                        <a class="card-title" href="{{ $data['list_route'] }}"> 
+                        {{ $data['lang']['change_password'] }}
+                        </a>
+                    </h5>
+                </div>
 
-                    <div class="card-body"> 
-                        <changepassword-view :module="{{json_encode($data)}}"></changepassword-view>
-                    </div>
+                <div class="card-body"> 
+                    <changepassword-view :module="{{json_encode($data)}}"></changepassword-view>
                 </div>
             </div>
-        </div>            
-    </section>
+        </div>
+    </div>            
 </div> 
 @endsection
