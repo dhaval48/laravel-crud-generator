dedae79989e1bf93b0660a7a6538b5e9aedbe5f4
Dhaval <dhaval48@gmail.com>
2019-05-08 18:17:31 +0530

    remove section from view blade php

--- Vuesample/view.php
+++ Vuesample/view.php
@@ -2,34 +2,32 @@
 
 @section('content')
 <div class="container">
-    <section>
-        <div class="row justify-content-center">
-            <div class="col-md-12 mtop-10">
-                <div class="card">
-                
-                @if(!isset($data['lists'])) 
-                    <div class="card-header">
-                        <h5>
-                            <a class="card-title" href="{{ $data['list_route'] }}"> 
-                                {{ $data['id'] != 0 ? $data['lang']['edit_title'] : $data['lang']['create_title'] }}
-                            </a>
-                        </h5>
-                    </div>
-
-                @else
-
-                    <h5><div class="card-header">{{ $data['lang']['list'] }}</div></h5>                   
-                   
-                @endif
-                    <div class="card-body">
-                        
-                        <[TNAME]-view :module="{{json_encode($data)}}"></[TNAME]-view>
-                        
-                    </div> 
+    <div class="row justify-content-center">
+        <div class="col-md-12 mtop-10">
+            <div class="card">
+            
+            @if(!isset($data['lists'])) 
+                <div class="card-header">
+                    <h5>
+                        <a class="card-title" href="{{ $data['list_route'] }}"> 
+                            {{ $data['id'] != 0 ? $data['lang']['edit_title'] : $data['lang']['create_title'] }}
+                        </a>
+                    </h5>
                 </div>
+
+            @else
+
+                <h5><div class="card-header">{{ $data['lang']['list'] }}</div></h5>                   
+               
+            @endif
+                <div class="card-body">
+                    
+                    <[TNAME]-view :module="{{json_encode($data)}}"></[TNAME]-view>
+                    
+                </div> 
             </div>
-        </div>  
-    </section>
+        </div>
+    </div>  
 </div>
 
 {{-- [Modal_path] --}}
